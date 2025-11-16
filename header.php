dc96461ef4c345325db02eee29e2fc2858c4f0ae
DawidZ2003 <mamba75210@interia.pl>
2025-11-16 16:42:22 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);  /* Ta linia musi być pierwsza */ ?>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<header>
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top mt-0 mb-0 ms-0 me-0 pt-0 pb-0 ps-0 pe-0">
+	<div class="container-fluid">
+	
+		<div class="collapse navbar-collapse" id="main_nav">
+			<ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="wyloguj.php">
+                       <i class="fa fa-sign-out"></i> Sign-out
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+			</ul> 
+		</div> 
+		
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		
+	</div> 
+</nav>		
+		
+</div>
+</header>
